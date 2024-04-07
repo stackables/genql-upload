26bb79180b56df20699f6ee9113cc89705f73b42
Aarne Laur <aarne.laur@gmail.com>
2024-04-07 11:15:36 +0200

    chore: Slimmer devDependencies (#123)

--- test/server.ts
+++ test/server.ts
@@ -1,21 +1,17 @@
-import { ApolloServer } from "@apollo/server";
-import { koaMiddleware } from "@as-integrations/koa";
-import { GraphQLUpload, graphqlUploadKoa } from "graphql-upload-ts";
-import http from "http";
-import Koa from "koa";
-import bodyParser from "koa-bodyparser";
+import { createSchema, createYoga } from "graphql-yoga";
+import { createServer } from "http";
+import { Readable } from "node:stream";
 import { finished } from "stream/promises";
 
 const typeDefs = `#graphql
 	# The implementation for this scalar is provided by the
 	# 'GraphQLUpload' export from the 'graphql-upload' package
 	# in the resolver map below.
-	scalar Upload
+	scalar File
 
-	type File {
+	type FileOutput {
 		filename: String!
 		mimetype: String!
-		encoding: String!
 		headers: String!
 	}
 
@@ -28,23 +24,23 @@ const typeDefs = `#graphql
 
 	type Mutation {
 		# Multiple uploads are supported. See graphql-upload docs for details.
-		singleUpload(file: Upload!): File!
+		singleUpload(file: File!): FileOutput!
 	}
 `;
 
 const resolvers = {
-	// This maps the `Upload` scalar to the implementation provided
-	// by the `graphql-upload` package.
-	Upload: GraphQLUpload,
-
 	Mutation: {
-		singleUpload: async (parent: any, args: any, context: any) => {
-			const { createReadStream, filename, mimetype, encoding } =
-				await args.file;
+		singleUpload: async (
+			parent: any,
+			args: { file: File },
+			context: { request: Request }
+		) => {
+			const payload = {
+				filename: args.file.name,
+				mimetype: args.file.type,
+			};
 
-			// Invoking the `createReadStream` will return a Readable Stream.
-			// See https://nodejs.org/api/stream.html#stream_readable_streams
-			const stream = createReadStream();
+			const stream = Readable.from(args.file.stream());
 
 			// This is purely for demonstration purposes and will overwrite the
 			// local-file-output.txt in the current working directory on EACH upload.
@@ -52,32 +48,24 @@ const resolvers = {
 			stream.pipe(out);
 			await finished(out);
 
-			const headers = JSON.stringify(context.headers);
+			const headers = JSON.stringify(
+				Object.fromEntries([...context.request.headers.entries()])
+			);
 
-			return { filename, mimetype, encoding, headers };
+			return { ...payload, headers };
 		},
 	},
 };
 
 export async function startServer() {
-	const app = new Koa();
-	const httpServer = http.createServer(app.callback());
-
-	// Set up Apollo Server
-	const server = new ApolloServer({
-		typeDefs,
-		resolvers,
+	const yoga = createYoga({
+		schema: createSchema({
+			typeDefs,
+			resolvers,
+		}),
 	});
-	await server.start();
 
-	app
-		.use(bodyParser())
-		.use(graphqlUploadKoa({ maxFileSize: 10 * 1000 * 1000, maxFiles: 10 }))
-		.use(
-			koaMiddleware(server, {
-				context: async ({ ctx }) => ({ headers: ctx.headers }),
-			})
-		);
+	const httpServer = createServer(yoga);
 
 	await new Promise((resolve) =>
 		httpServer.listen({ port: 4000 }, () => resolve(true))
